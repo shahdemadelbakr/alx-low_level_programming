6c3a3449a0e4cff0e23f0f0df23e6240bf6ea120
shahdemadelbakr <shahdelbkr@gmail.com>
2023-07-14 16:52:52 -0700

    ddddddd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main -A prograam that prints aline using the printsf function
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\*n")
+		return (0);
+}
