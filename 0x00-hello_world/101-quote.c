2f64406184dc3a21708ac8d3faa7449188d7ff26
shahdemadelbakr <shahdelbkr@gmail.com>
2023-07-15 14:17:40 -0700

    ddddddd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main -A c programm that prints a line to the standard error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is usefu\l" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
